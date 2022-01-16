d8c135896148d8507911e4ba4f22d72a27829d0b
Matej Konrad <konrad.matej@gmail.com>
2022-01-16 22:23:44 +0100

    feat: Sketch colors update

--- sketch-colors/sketch.js
+++ sketch-colors/sketch.js
@@ -1,17 +1,23 @@
 let step = 3;
 function setup() {
-    createCanvas(720, 480);
+    createCanvas(windowWidth, windowHeight);
     strokeWeight(step);
     ellipseMode(RADIUS);
+    background(0)
 }
 
 let counter = 0;
+let counter2 = 0;
 function draw() {
     stroke(random(255), random(255), random(255));
-
-    line(counter, 0, counter, height);
+    fill(100);
+    circle(counter, counter, counter2, counter2)
+    counter2+=1;
     counter+=3;
     if (counter > width) {
         counter = 0;
+    }
+    if (counter2 >  10) { 
+      coutner = 0 
     }
 }
